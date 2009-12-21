4b04f249a153a9deff35154ce51882ee1ca7c4db
George Kettleborough <g.kettleborough@member.fsf.org>
2009-12-21 23:05:29 +0000

    Change poly array to coefficient array

---
+++ coef_array.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "coef_array.h"
+#include "polynomial.h"
+
+static const unsigned default_size = 10;
+
+CoefArray new_coef_array()
+{
+     CoefArray array;
+     array.size = 0;
+     array.max_size = default_size;
+     array.head = malloc(sizeof(Coefficient)*default_size);
+     return array;
+}
+
+CoefPtr ca_push_back(CoefArray *ca, Coefficient nc)
+{
+     Coefficient *new_head;
+     unsigned i;
+     
+     if (ca->size == ca->max_size) {
+          /* double array size */
+          ca->max_size = ca->max_size *2;
+          new_head = malloc(sizeof(Coefficient)*ca->max_size);
+          for (i = 0; i < ca->size; ++i) {
+               *(new_head+i) = *(ca->head+i);
+          }
+          free(ca->head);
+          ca->head = new_head;
+          new_head = NULL;
+     }
+
+     (ca->head + ca->size)->type = special;
+     copy_coefficient(ca->head + ca->size, nc);
+
+     return ca->head + ca->size++;
+}
+
+CoefPtr ca_poly_push_back(CoefArray *ca, Polynomial np)
+{
+     Coefficient nc;
+     nc.type = polynomial;
+     nc.u.poly = np;
+     return ca_push_back(ca, nc);
+}
+
+CoefPtr ca_rational_push_back(CoefArray *ca, BigRat nr)
+{
+     Coefficient nc;
+     nc.type = rational;
+     nc.u.rat = nr;
+     return ca_push_back(ca, nc);
+}
+
+Coefficient ca_get(CoefArray *ca, unsigned index)
+{
+     return *(ca->head + index);
+}
+
---
+++ coef_array.h
@@ -0,0 +1,29 @@
+/* dynamic arrays of coefficients are necessary for polynomial
+ * remainder sequences and factorisations */
+
+#ifndef _COEF_ARRAY_H_
+#define _COEF_ARRAY_H_
+
+#include "polynomial.h"
+#include "bigrat.h"
+
+typedef struct coef_array
+{
+     unsigned size;
+     unsigned max_size;
+     Coefficient *head;
+} CoefArray;
+
+/* make a new empty array with default max size */
+CoefArray new_coef_array();
+/* put a copy of coefficient at end of array */
+CoefPtr ca_push_back(CoefArray *ca, Coefficient nc);
+/* put a copy of a polynomial or rational at end */
+CoefPtr ca_poly_push_back(CoefArray *ca, Polynomial np);
+CoefPtr ca_rational_push_back(CoefArray *ca, BigRat nr);
+/* get a polynomial from array, it is an error to use an invalid index
+ * ie. index must be < size */
+Coefficient ca_get(CoefArray *ca, unsigned index);
+
+
+#endif /* _COEF_ARRAY_H_ */
