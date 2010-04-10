fe76d60bc677726774c3228127cc902f657e5bf0
George Kettleborough <g.kettleborough@member.fsf.org>
2010-04-10 01:32:28 +0100

    Make squarefree factorisation work for multivariate polys

--- src/squarefree.c
+++ src/squarefree.c
@@ -9,103 +9,88 @@
 #include "squarefree.h"
 #include "tree.h"
 
-void Squarefree(CoefArray *f, Polynomial A)
+void Squarefree(CoefArray *f, Coefficient A, char var)
 {
      Coefficient c = {special};
      Coefficient *Ak = NULL;
-     Polynomial S = {'x', NULL},Sd = {'x', NULL},Sm = {'x', NULL};
-     Polynomial Ss = {'x', NULL}, Ssd = {'x', NULL}, Y = {'x', NULL};
-     Polynomial Z = {'x', NULL}, At = {'x', NULL};
-     /* unsigned i = 1; */
+     Coefficient S = {special},Sd = {special},Sm = {special};
+     Coefficient Ss = {special}, Ssd = {special}, Y = {special};
+     Coefficient Z = {special}, At = {special};
+     unsigned i = 1;
 
      /* c <-- content(A) */
-     poly_content(&c, A);
-     /* printf("c = "); */
-     /* print_coefficient(c); */
-     /* printf("\n"); */
+     coef_content(&c, A, var);
+     PRINTC(c);
      /* S <-- pp(A) */
-     poly_pp(&S, A);
-     poly_differentiate(&Sd, S);
-     /* printf("S' = "); */
-     /* print_poly(Sd); */
-     /* printf("\n"); */
-     SubResultantGCD(&Sm, S, Sd);
-     /* printf("S- = "); */
-     /* print_poly(Sm); */
-     /* printf("\n"); */
-     exact_div_polynomials(&Ss, S, Sm);
-     exact_div_polynomials(&Y, Sd, Sm);
-     /* printf("Y = "); */
-     /* print_poly(Y); */
-     /* printf("\n"); */
+     exact_div_coefficients(&S, A, c);
+     coef_differentiate(&Sd, S, var);
+     PRINTC(Sd);
+     coef_gcd(&Sm, S, Sd);
+     PRINTC(Sm);
+     exact_div_coefficients(&Ss, S, Sm);
+     exact_div_coefficients(&Y, Sd, Sm);
+     PRINTC(Y);
 
-     poly_differentiate(&Ssd, Ss);
-     /* printf("S*' = "); */
-     /* print_poly(Ssd); */
-     /* printf("\n"); */
-     sub_polynomials(&Z, Y, Ssd);
-     while (!poly_zero(Z)) {
-          SubResultantGCD(&At, Ss, Z);
-          Ak = ca_poly_push_back(f, At);
+     coef_differentiate(&Ssd, Ss, var);
+     PRINTC(Ssd);
+     sub_coefficients(&Z, Y, Ssd);
+     while (!coef_zero(Z)) {
+          coef_gcd(&At, Ss, Z);
+          PRINTC(At);
+          Ak = ca_push_back(f, At);
           
-          /* printf("%d\n", i++); */
-          /* printf("S* = "); */
-          /* print_poly(Ss); */
-          /* printf("\nY = "); */
-          /* print_poly(Y); */
-          /* printf("\nZ = "); */
-          /* print_poly(Z); */
-          /* printf("\nA_k = "); */
-          /* print_poly(Ak->u.poly); */
-          /* printf("\n"); */
+          printf("%d\n", i++);
+          PRINTC(Ss);
+          PRINTC(Y);
+          PRINTC(Z);
+          PRINTC(*Ak);
           
-          exact_div_polynomials(&Ss, Ss, Ak->u.poly);
-          exact_div_polynomials(&Y, Z, Ak->u.poly);
+          exact_div_coefficients(&Ss, Ss, *Ak);
+          exact_div_coefficients(&Y, Z, *Ak);
 
-          poly_differentiate(&Ssd, Ss);
-          sub_polynomials(&Z, Y, Ssd);
+          coef_differentiate(&Ssd, Ss, var);
+          sub_coefficients(&Z, Y, Ssd);
      }
 
-     /* printf("%d\n", i++); */
-     /* printf("S* = "); */
-     /* print_poly(Ss); */
-     /* printf("\nY = "); */
-     /* print_poly(Y); */
-     /* printf("\nZ = "); */
-     /* print_poly(Z); */
-     /* printf("\nA_k = "); */
-     /* print_poly(Ss); */
-     /* printf("\n"); */
+     printf("%d\n", i++);
+     PRINTC(Ss);
+     PRINTC(Y);
+     PRINTC(Z);
+     if (Ak) {
+          PRINTC(*Ak);
+     }
           
-     ca_poly_push_back(f, Ss);
+     ca_push_back(f, Ss);
      mul_coefficients(f->head, *f->head, c);
 }
 
-void SquarefreeI(node_type *root)
+void SquarefreeI(node_type *root, char var)
 {
      CoefArray arr;
      int i;
      
      /* input must be one polynomial */
-     if (root->type != poly_type) {
+     if (root->type != coef_type) {
           printf("Error. Input to Squarefree must be a polynomial.\n");
           return;
      }
 
      arr = new_coef_array();
 
-     Squarefree(&arr, root->u.poly);
-
+     Squarefree(&arr, root->u.coef, var);
+     printf("...\n");
+     print_coefficient_nonpretty(root->u.coef);
+     printf(" = ");
      for (i = 0; i < arr.size; ++i) {
           if (coef_one(ca_get(&arr, i))) {
                continue;
           }
-          print_coefficient(ca_get(&arr, i));
+          print_coefficient_nonpretty(ca_get(&arr, i));
           if (i > 0) {
                printf("^%u", i+1);
           }
           if (i != arr.size-1) {
-               printf("*");
+               printf(" * ");
           }
           else {
                printf("\n");
@@ -114,4 +99,3 @@ void SquarefreeI(node_type *root)
 
      ca_free(&arr);
 }
-
--- src/squarefree.h
+++ src/squarefree.h
@@ -6,8 +6,8 @@
 #include "coef_array.h"
 #include "tree.h"
 
-void Squarefree(CoefArray *f, Polynomial A);
+void Squarefree(CoefArray *f, Coefficient A, char var);
 
-void SquarefreeI(node_type *root);
+void SquarefreeI(node_type *root, char var);
 
 #endif /* _SQUAREFREE_H_ */
